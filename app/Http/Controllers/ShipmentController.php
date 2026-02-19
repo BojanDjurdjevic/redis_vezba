61a4e991f396da360f0bb9d9119ff4ecb49e6d2a
BojanDjurdjevic <pininfarina164@gmail.com>
2026-02-19 22:52:10 +0100

    Domaći 30 -> Gates for create-update shipment forms added

--- app/Http/Controllers/ShipmentController.php
+++ app/Http/Controllers/ShipmentController.php
@@ -34,6 +34,7 @@ class ShipmentController extends Controller
      */
     public function create()
     {
+        Gate::authorize('create', Shipment::class);
         $users = User::all();
         return view('shipments.create', compact('users'));
     }
@@ -103,6 +104,7 @@ class ShipmentController extends Controller
      */
     public function edit(Shipment $shipment)
     {
+        Gate::authorize('create', Shipment::class);
         $users = User::all();
         return view('shipments.edit', compact('shipment', 'users'));
     }
@@ -112,6 +114,7 @@ class ShipmentController extends Controller
      */
     public function update(UpdateShipmentRequest $request, Shipment $shipment)
     {
+        Gate::authorize('create', Shipment::class);
         $shipment->update($request->validated());
 
         return redirect()->back()->with('success', 'Uspešno ste izmenili pošiljku!');
